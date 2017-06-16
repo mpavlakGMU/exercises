d899329c4a8b86b4a69493c23d0b157a202316ba
joinr <vangelder@tantalus.net>
2017-06-16 13:23:07 -0500

    added reductions examples

---
+++ src/exercises/blah.clj
@@ -0,0 +1,46 @@
+(ns exercises.blah)
+
+(def m {4 #{2}, 9 #{3}})
+
+;;original
+(reduce (fn [acc n]
+          (update acc 4 conj (rand-int 1000)))
+        m
+        (range 10))
+
+;;meta data tracks previous history
+(reduce (fn [acc n]
+          (let [res (update acc 4 conj (rand-int 1000))]
+            (vary-meta res
+                       #(update % :history conj acc))))
+        (with-meta m {:history []})
+        (range 10))
+
+;;intermediate values via reductions
+(reductions (fn [acc n]
+          (update acc 4 conj (rand-int 1000)))
+        m
+        (range 10))
+
+;;using state
+(let [history (atom [])
+      res     (reduce (fn [acc n]
+                        (do (swap! history conj acc)
+                            (update acc 4 conj (rand-int 1000))))
+                  m
+                  (range 10))]
+  [res (conj @history res)])
+
+;;most idiomatic
+(reduce (fn [[acc history] n]
+          [(update acc 4 conj (rand-int 1000))
+           (conj history acc)])
+        [m [m]]
+        (range 10))
+
+(reduce (fn [acc n]
+          (let [[res history] acc]
+            [(update res 4 conj (rand-int 1000))
+             (conj history res)])
+          [m [m]])
+        (range 10))
